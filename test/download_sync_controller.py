80df5c89086431bfc23042dcfef6974bc7ca5051
Regis Merlino <regis.merlino@intel.com>
2012-11-30 17:30:47 +0100

    [Tests] Add a simple download sync controller test app
    Signed-off-by: Regis Merlino <regis.merlino@intel.com>

---
+++ test/download_sync_controller.py
@@ -0,0 +1,346 @@
+# download_sync_controller
+#
+# Copyright (C) 2012 Intel Corporation. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU Lesser General Public License,
+# version 2.1, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Regis Merlino <regis.merlino@intel.com>
+#
+
+import ConfigParser
+import os
+import dbus
+
+from mediaconsole import UPNP, Container, Device
+
+class _DscUpnp(UPNP):
+    def __init__(self):
+        UPNP.__init__(self)
+
+    def get_servers(self):
+        return self._manager.GetServers()
+
+class _DscContainer(Container):
+    def __init__(self, path):
+        Container.__init__(self, path)
+        self.__path = path
+
+    def find_containers(self):
+            return self._containerIF.SearchObjectsEx(
+                                        'Type derivedfrom "container"',
+                                        0, 0,
+                                        ['DisplayName', 'Path', 'Type'], '')[0]
+    def find_updates(self, upd_id):
+            return self._containerIF.SearchObjectsEx(
+                            'ObjectUpdateID > "{0}"'.format(upd_id),
+                            0, 0,
+                            ['DisplayName', 'Path', 'Type', 'Parent'], '')[0]
+
+    def find_children(self):
+        return self._containerIF.ListChildrenEx(0, 0,
+                                             ['DisplayName', 'Path', 'Parent',
+                                              'Type'], '')
+
+class DscError(Exception):
+    def __init__(self, str):
+        self.str = str
+
+    def __str__(self):
+        return 'DscError: ' + self.str
+
+class _DscStore(object):
+    ITEM_NEW = 1
+    ITEM_UPDATE = 2
+    CONTAINER_NEW = 3
+
+    def __init__(self, root_path, server_id):
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)
+
+        self.__config_path = root_path + '/' + server_id + '.conf'
+        self.__config = ConfigParser.ConfigParser()
+        self.__config.read(self.__config_path)
+
+    def __write_config(self):
+        with open(self.__config_path, 'wb') as configfile:
+            self.__config.write(configfile)
+
+    def __id_from_path(self, path):
+        return os.path.basename(path)
+
+    def __orig_id(self, media_object):
+        try:
+            return self.__id_from_path(media_object['RefPath'])
+        except KeyError:
+            return self.__id_from_path(media_object['Path'])
+
+    def __removed_items(self, local_ids, remote_items):
+        for local_id in local_ids:
+            found = False
+
+            for remote in remote_items:
+                remote_id = self.__orig_id(remote)
+                if local_id == remote_id:
+                    found = True
+
+            if not found:
+                yield local_id
+
+    def __sync_item(self, obj, obj_id, parent_id, status, write_conf):
+        if status == _DscStore.ITEM_UPDATE:
+            print u'\tMedia "{0}" updated'.format(obj['DisplayName'])
+        elif status == _DscStore.ITEM_NEW:
+            print u'\tNew media "{0}" tracked'.format(obj['DisplayName'])
+        else:
+            pass
+
+        self.__config.set(parent_id, obj_id, obj['DisplayName'])
+        
+        if write_conf:
+            self.__write_config()
+
+    def reset(self):
+        if os.path.exists(self.__config_path):
+            os.remove(self.__config_path)
+
+        self.__config = ConfigParser.ConfigParser()
+
+    def sync_container(self, container, items):
+        print u'Syncing container  {0}...'.format(container['DisplayName'])
+
+        container_id = self.__id_from_path(container['Path'])
+        if not self.__config.has_section(container_id):
+            self.__config.add_section(container_id)
+
+        for remote in items:
+            remote_id = self.__id_from_path(remote['Path'])
+            if not self.__config.has_option(container_id, remote_id):
+                if remote['Type'] == 'container':
+                    status = _DscStore.CONTAINER_NEW
+                else:
+                    status = _DscStore.ITEM_NEW
+                self.__sync_item(remote, remote_id, container_id, status, False)
+
+        for local in self.__removed_items(
+                                    self.__config.options(container_id), items):
+            if self.__config.has_section(local):
+                print u'\tRemoved container "{0}"' \
+                                .format(self.__config.get(container_id, local))
+                self.__config.remove_option(container_id, local)
+                self.__config.remove_section(local)
+            else:
+                print u'\tRemoved media "{0}"' \
+                                .format(self.__config.get(container_id, local))
+                self.__config.remove_option(container_id, local)
+
+        self.__write_config()
+
+    def sync_item(self, obj):
+        obj_id = self.__id_from_path(obj['Path'])
+        parent_id = self.__id_from_path(obj['Parent'])
+        if self.__config.has_option(parent_id, obj_id):
+            status = _DscStore.ITEM_UPDATE
+        else:
+            status = _DscStore.ITEM_NEW
+
+        self.__sync_item(obj, obj_id, parent_id, status, True)
+
+class DscController(object):
+    CONFIG_PATH = os.environ['HOME'] + '/.config/download-sync-controller.conf'
+    SUID_OPTION = 'system_update_id'
+    SRT_OPTION = 'service_reset_token'
+    DATA_PATH_SECTION = '__data_path__'
+    DATA_PATH_OPTION = 'path'
+
+    def __init__(self, rel_path = None):
+        
+        self.__upnp = _DscUpnp()
+
+        self.__config = ConfigParser.ConfigParser()
+        self.__config.read(DscController.CONFIG_PATH)
+        
+        if rel_path:
+            self.__set_data_path(rel_path)
+        elif not self.__config.has_section(DscController.DATA_PATH_SECTION):
+            self.__set_data_path('download-sync-controller')
+            
+        self.__store_path = self.__config.get(DscController.DATA_PATH_SECTION,
+                                                DscController.DATA_PATH_OPTION)
+
+    def __write_config(self):
+        with open(DscController.CONFIG_PATH, 'wb') as configfile:
+            self.__config.write(configfile)
+
+    def __set_data_path(self, rel_path):
+        data_path = os.environ['HOME'] + '/' + rel_path
+
+        if not self.__config.has_section(DscController.DATA_PATH_SECTION):
+            self.__config.add_section(DscController.DATA_PATH_SECTION)
+
+        self.__config.set(DscController.DATA_PATH_SECTION,
+                                    DscController.DATA_PATH_OPTION, data_path)
+
+        self.__write_config()
+
+    def __need_sync(self, servers):
+        for item in servers:
+            device = Device(item)
+            uuid = device.get_prop('UDN')
+            new_srt = device.get_prop('ServiceResetToken')
+            new_id = device.get_prop('SystemUpdateID')
+
+            if self.__config.has_section(uuid):
+                cur_id = self.__config.getint(uuid, DscController.SUID_OPTION)
+                cur_srt = self.__config.get(uuid, DscController.SRT_OPTION)
+                if cur_id == -1 or cur_srt != new_srt:
+                    print
+                    print u'Server {0} needs *full* sync:'.format(uuid)
+                    yield item, uuid, 0, new_id, True
+                elif cur_id < new_id:
+                    print
+                    print u'Server {0} needs sync:'.format(uuid)
+                    yield item, uuid, cur_id, new_id, False
+
+    def __check_trackable(self, server):
+        try:
+            try:
+                srt = server.get_prop('ServiceResetToken')
+            except:
+                raise DscError("'ServiceResetToken' variable not supported")
+
+            try:
+                dlna_caps = server.get_prop('DLNACaps')
+                content_sync_cap = dlna_caps['content-synchronization']
+                if not content_sync_cap:
+                    raise
+            except:
+                raise DscError("'content-synchronization' cap not supported")
+
+            try:
+                search_caps = server.get_prop('SearchCaps')
+                if not [x for x in search_caps if 'objectUpdateID' in x]:
+                    raise
+            except:
+                raise DscError("'objectUpdateID' search cap not supported")
+
+            return srt
+        except DscError as err:
+            print err
+            return None
+    
+    def track(self, server_path, track = True):
+        server = Device(server_path)
+        server_uuid = server.get_prop('UDN')
+        
+        if track == True and not self.__config.has_section(server_uuid):
+            srt = self.__check_trackable(server)
+            if srt != None:
+                self.__config.add_section(server_uuid)
+
+                self.__config.set(server_uuid, DscController.SUID_OPTION, '-1')
+                self.__config.set(server_uuid, DscController.SRT_OPTION, srt)
+                
+                self.__write_config()
+            else:
+                print u"Sorry, the server {0} has no such capability and " \
+                        "will not be tracked.".format(server_path)
+
+        elif track == False and self.__config.has_section(server_uuid):
+            self.__config.remove_section(server_uuid)
+            self.__write_config()
+            
+            store = _DscStore(self.__store_path, server_uuid)
+            store.reset()
+
+    def track_reset(self, server):
+        self.track(server, False)
+        self.track(server)
+
+    def servers(self):
+        print u'Running servers:'
+
+        for item in self.__upnp.get_servers():
+            try:
+                server = Container(item)
+                try:
+                    folder_name = server.get_prop('FriendlyName')
+                except Exception:
+                    folder_name = server.get_prop('DisplayName')
+                device = Device(item)
+                dev_uuid = device.get_prop('UDN')
+                dev_path = device.get_prop('Path')
+
+                print u'{0:<25}  Tracked({2})  {3}  {1}'.format(folder_name,
+                                            dev_path,
+                                            self.__config.has_option(dev_uuid,
+                                                    DscController.SUID_OPTION),
+                                            dev_uuid)
+
+            except dbus.exceptions.DBusException as err:
+                print u'Cannot retrieve properties for ' + item
+                print str(err).strip()[:-1]
+
+    def tracked_servers(self):
+        print u'Tracked servers:'
+
+        for name in self.__config.sections():
+            if name != DscController.DATA_PATH_SECTION:
+                print u'{0:<30}'.format(name)
+
+    def sync(self):
+        print u'Syncing...'
+
+        for item, uuid, cur, new, full_sync in \
+                                    self.__need_sync(self.__upnp.get_servers()):
+            store = _DscStore(self.__store_path, uuid)
+
+            if full_sync:
+                print u'Resetting local contents for server {0}'.format(uuid)
+
+                self.track_reset(item)
+                store.reset()
+
+                objects = _DscContainer(item).find_containers()
+            else:
+                objects = _DscContainer(item).find_updates(cur)
+                
+            for obj in objects:
+                if obj['Type'] == 'container':
+                    children = _DscContainer(obj['Path']).find_children()
+                    store.sync_container(obj, children)
+                else:
+                    store.sync_item(obj)
+
+            self.__config.set(uuid, DscController.SUID_OPTION, str(new))
+            self.__write_config()
+
+        print
+        print u'Done.'
+
+    def reset(self):
+        for name in self.__config.sections():
+            if name != DscController.DATA_PATH_SECTION:
+                self.__config.remove_section(name)
+
+                store = _DscStore(self.__store_path, name)
+                store.reset()
+
+        self.__write_config()
+
+
+if __name__ == '__main__':
+    controller = DscController()
+    controller.servers()
+    print
+    print u'"controller" instance is ready for use.'
--- test/mediaconsole.py
+++ test/mediaconsole.py
@@ -29,7 +29,7 @@ class MediaObject(object):
 
     def __init__(self, path):
         bus = dbus.SessionBus()
-        self.__propsIF = dbus.Interface(bus.get_object(
+        self._propsIF = dbus.Interface(bus.get_object(
                 'com.intel.media-service-upnp', path),
                                         'org.freedesktop.DBus.Properties')
         self.__objIF = dbus.Interface(bus.get_object(
@@ -37,19 +37,19 @@ class MediaObject(object):
                                       'org.gnome.UPnP.MediaObject2')
 
     def get_props(self, iface = ""):
-        return self.__propsIF.GetAll(iface)
+        return self._propsIF.GetAll(iface)
 
     def get_prop(self, prop_name, iface = ""):
-        return self.__propsIF.Get(iface, prop_name)
+        return self._propsIF.Get(iface, prop_name)
 
     def print_prop(self, prop_name, iface = ""):
-        print_properties(self.__propsIF.Get(iface, prop_name))
+        print_properties(self._propsIF.Get(iface, prop_name))
 
     def print_props(self, iface = ""):
-        print_properties(self.__propsIF.GetAll(iface))
+        print_properties(self._propsIF.GetAll(iface))
 
     def print_dms_id(self):
-        path = self.__propsIF.Get("", "Path")
+        path = self._propsIF.Get("", "Path")
         dms_id = path[path.rfind("/") + 1:]
         i = 0
         while i+1 < len(dms_id):
@@ -68,12 +68,12 @@ class Item(MediaObject):
     def __init__(self, path):
         MediaObject.__init__(self, path)
         bus = dbus.SessionBus()
-        self.__itemIF = dbus.Interface(bus.get_object(
+        self._itemIF = dbus.Interface(bus.get_object(
                 'com.intel.media-service-upnp', path),
                                        'org.gnome.UPnP.MediaItem2')
 
     def print_compatible_resource(self, protocol_info, fltr):
-        print_properties(self.__itemIF.GetCompatibleResource(protocol_info,
+        print_properties(self._itemIF.GetCompatibleResource(protocol_info,
                                                              fltr))
 
 class Container(MediaObject):
@@ -81,30 +81,30 @@ class Container(MediaObject):
     def __init__(self, path):
         MediaObject.__init__(self, path)
         bus = dbus.SessionBus()
-        self.__containerIF = dbus.Interface(bus.get_object(
+        self._containerIF = dbus.Interface(bus.get_object(
                 'com.intel.media-service-upnp', path),
                                             'org.gnome.UPnP.MediaContainer2')
 
     def list_children(self, offset, count, fltr, sort=""):
-        objects = self.__containerIF.ListChildrenEx(offset, count, fltr, sort)
+        objects = self._containerIF.ListChildrenEx(offset, count, fltr, sort)
         for item in objects:
             print_properties(item)
             print ""
 
     def list_containers(self, offset, count, fltr, sort=""):
-        objects = self.__containerIF.ListContainersEx(offset, count, fltr, sort)
+        objects = self._containerIF.ListContainersEx(offset, count, fltr, sort)
         for item in objects:
             print_properties(item)
             print ""
 
     def list_items(self, offset, count, fltr, sort=""):
-        objects = self.__containerIF.ListItemsEx(offset, count, fltr, sort)
+        objects = self._containerIF.ListItemsEx(offset, count, fltr, sort)
         for item in objects:
             print_properties(item)
             print ""
 
     def search(self, query, offset, count, fltr, sort=""):
-        objects, total = self.__containerIF.SearchObjectsEx(query, offset,
+        objects, total = self._containerIF.SearchObjectsEx(query, offset,
                                                             count, fltr, sort)
         print "Total Items: " + str(total)
         print
@@ -113,7 +113,7 @@ class Container(MediaObject):
             print ""
 
     def tree(self, level=0):
-        objects = self.__containerIF.ListChildren(
+        objects = self._containerIF.ListChildren(
             0, 0, ["DisplayName", "Path", "Type"])
         for props in objects:
             print (" " * (level * 4) + props["DisplayName"] +
@@ -122,12 +122,12 @@ class Container(MediaObject):
                 Container(props["Path"]).tree(level + 1)
 
     def upload(self, name, file_path):
-        (tid, path) = self.__containerIF.Upload(name, file_path)
+        (tid, path) = self._containerIF.Upload(name, file_path)
         print "Transfer ID: " + str(tid)
         print "Path: " + path
 
     def create_container(self, name, type, child_types):
-        path = self.__containerIF.CreateContainer(name, type, child_types)
+        path = self._containerIF.CreateContainer(name, type, child_types)
         print "New container path: " + path
 
 class Device(Container):
@@ -135,47 +135,47 @@ class Device(Container):
     def __init__(self, path):
         Container.__init__(self, path)
         bus = dbus.SessionBus()
-        self.__deviceIF = dbus.Interface(bus.get_object(
+        self._deviceIF = dbus.Interface(bus.get_object(
                 'com.intel.media-service-upnp', path),
                                          'com.intel.UPnP.MediaDevice')
 
     def upload_to_any(self, name, file_path):
-        (tid, path) = self.__deviceIF.UploadToAnyContainer(name, file_path)
+        (tid, path) = self._deviceIF.UploadToAnyContainer(name, file_path)
         print "Transfer ID: " + str(tid)
         print "Path: " + path
 
     def create_container_in_any(self, name, type, child_types):
-        path = self.__deviceIF.CreateContainerInAnyContainer(name, type,
+        path = self._deviceIF.CreateContainerInAnyContainer(name, type,
                                                              child_types)
         print "New container path: " + path
 
     def get_upload_status(self, id):
-        (status, length, total) = self.__deviceIF.GetUploadStatus(id)
+        (status, length, total) = self._deviceIF.GetUploadStatus(id)
         print "Status: " + status
         print "Length: " + str(length)
         print "Total: " + str(total)
 
     def get_upload_ids(self):
-        upload_ids  = self.__deviceIF.GetUploadIDs()
+        upload_ids  = self._deviceIF.GetUploadIDs()
         print_properties(upload_ids)
 
     def cancel_upload(self, id):
-        self.__deviceIF.CancelUpload(id)
+        self._deviceIF.CancelUpload(id)
 
     def cancel(self):
-        return self.__deviceIF.Cancel()
+        return self._deviceIF.Cancel()
 
 class UPNP(object):
 
     def __init__(self):
         bus = dbus.SessionBus()
-        self.__manager = dbus.Interface(bus.get_object(
+        self._manager = dbus.Interface(bus.get_object(
                 'com.intel.media-service-upnp',
                 '/com/intel/MediaServiceUPnP'),
                                         'com.intel.MediaServiceUPnP.Manager')
 
     def servers(self):
-        for i in self.__manager.GetServers():
+        for i in self._manager.GetServers():
             try:
                 server = Container(i)
                 try:
@@ -188,10 +188,10 @@ class UPNP(object):
                 print str(err).strip()[:-1]
 
     def version(self):
-        print self.__manager.GetVersion()
+        print self._manager.GetVersion()
 
     def set_protocol_info(self, protocol_info):
-        self.__manager.SetProtocolInfo(protocol_info)
+        self._manager.SetProtocolInfo(protocol_info)
 
     def prefer_local_addresses(self, prefer):
-        self.__manager.PreferLocalAddresses(prefer)
+        self._manager.PreferLocalAddresses(prefer)
