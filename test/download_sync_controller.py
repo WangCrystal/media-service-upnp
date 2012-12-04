621f46e08f1a476fd06df00f00c8178b833b92ad
Regis Merlino <regis.merlino@intel.com>
2012-12-04 18:38:19 +0100

    [Tests] Enhance download-sync-controller with more meta data and actual media download
    Signed-off-by: Regis Merlino <regis.merlino@intel.com>

--- test/download_sync_controller.py
+++ test/download_sync_controller.py
@@ -20,6 +20,8 @@
 
 import ConfigParser
 import os
+import shutil
+import urllib
 import dbus
 
 from mediaconsole import UPNP, Container, Device
@@ -37,40 +39,84 @@ class _DscContainer(Container):
         self.__path = path
 
     def find_containers(self):
-            return self._containerIF.SearchObjectsEx(
+        return self._containerIF.SearchObjectsEx(
                                         'Type derivedfrom "container"',
                                         0, 0,
                                         ['DisplayName', 'Path', 'Type'], '')[0]
     def find_updates(self, upd_id):
-            return self._containerIF.SearchObjectsEx(
+        return self._containerIF.SearchObjectsEx(
                             'ObjectUpdateID > "{0}"'.format(upd_id),
                             0, 0,
-                            ['DisplayName', 'Path', 'Type', 'Parent'], '')[0]
+                            ['DisplayName', 'Path', 'RefPath', 'URLs',
+                             'Type', 'Parent'], '')[0]
 
     def find_children(self):
         return self._containerIF.ListChildrenEx(0, 0,
-                                             ['DisplayName', 'Path', 'Parent',
-                                              'Type'], '')
+                                             ['DisplayName', 'Path', 'RefPath',
+                                              'URLs', 'Parent', 'Type'], '')
 
 class DscError(Exception):
-    def __init__(self, str):
-        self.str = str
+    def __init__(self, message):
+        Exception.__init__(self, message)
+        self.message = message
 
     def __str__(self):
-        return 'DscError: ' + self.str
+        return 'DscError: ' + self.message
+
+class DscDownloader(object):
+    def __init__(self, url, path):
+        self.url = url
+        self.path = path
+
+    def download(self):
+        urllib.urlretrieve(self.url, self.path)
 
 class _DscStore(object):
+    SYNC_SECTION = 'sync_info'
+    SYNC_OPTION = 'sync_contents'
+    CUR_ID_OPTION = 'current_id'
+    MEDIA_SECTION = 'media_info'
+    
+    NAME_SUFFIX = '-name'
+
     ITEM_NEW = 1
     ITEM_UPDATE = 2
     CONTAINER_NEW = 3
 
     def __init__(self, root_path, server_id):
-        if not os.path.exists(root_path):
-            os.makedirs(root_path)
+        self.__root_path = root_path + '/' + server_id
+        self.__config_path = self.__root_path + '/' + 'tracking.conf'
 
-        self.__config_path = root_path + '/' + server_id + '.conf'
         self.__config = ConfigParser.ConfigParser()
+        self.__cur_id = 0
+        self.__sync = False
+
+    def initialize(self, sync):
+        if not os.path.exists(self.__root_path):
+            os.makedirs(self.__root_path)
+
         self.__config.read(self.__config_path)
+
+        if not self.__config.has_section(_DscStore.SYNC_SECTION):
+            self.__config.add_section(_DscStore.SYNC_SECTION)
+            self.__config.set(_DscStore.SYNC_SECTION,
+                              _DscStore.CUR_ID_OPTION, '0')
+            if sync:
+                self.__config.set(_DscStore.SYNC_SECTION,
+                                  _DscStore.SYNC_OPTION, 'yes')
+            else:
+                self.__config.set(_DscStore.SYNC_SECTION,
+                                  _DscStore.SYNC_OPTION, 'no')
+
+
+        if not self.__config.has_section(_DscStore.MEDIA_SECTION):
+            self.__config.add_section(_DscStore.MEDIA_SECTION)
+
+        self.__cur_id = self.__config.getint(_DscStore.SYNC_SECTION,
+                                             _DscStore.CUR_ID_OPTION)
+
+        self.__sync = self.__config.getboolean(_DscStore.SYNC_SECTION,
+                                               _DscStore.SYNC_OPTION)
 
     def __write_config(self):
         with open(self.__config_path, 'wb') as configfile:
@@ -90,31 +136,50 @@ class _DscStore(object):
             found = False
 
             for remote in remote_items:
-                remote_id = self.__orig_id(remote)
-                if local_id == remote_id:
+                remote_id = self.__id_from_path(remote['Path'])
+                if local_id.endswith(_DscStore.NAME_SUFFIX) or \
+                                                        local_id == remote_id:
                     found = True
 
             if not found:
                 yield local_id
 
     def __sync_item(self, obj, obj_id, parent_id, status, write_conf):
+        orig = self.__orig_id(obj)
+
         if status == _DscStore.ITEM_UPDATE:
             print u'\tMedia "{0}" updated'.format(obj['DisplayName'])
+            self.__config.set(_DscStore.MEDIA_SECTION, orig, obj['DisplayName'])
         elif status == _DscStore.ITEM_NEW:
             print u'\tNew media "{0}" tracked'.format(obj['DisplayName'])
+            self.__config.set(parent_id, obj_id, orig)
+            self.__config.set(parent_id, obj_id + _DscStore.NAME_SUFFIX,
+                              obj['DisplayName'])
+            if obj_id == orig and self.__sync:
+                local_path = self.__create_path_for_name(obj['DisplayName'])
+                self.__config.set(_DscStore.MEDIA_SECTION, orig, local_path)
+                print u'\tDownloading contents from "{0}"'.format(obj['URLs'][0])
+                print u'\t\tinto "{0}"...'.format(local_path)
+                downloader = DscDownloader(obj['URLs'][0], local_path)
+                downloader.download()
         else:
             pass
 
-        self.__config.set(parent_id, obj_id, obj['DisplayName'])
-        
         if write_conf:
             self.__write_config()
 
-    def reset(self):
-        if os.path.exists(self.__config_path):
-            os.remove(self.__config_path)
+    def __create_path_for_name(self, file_name):
+        new_path = self.__root_path + '/' + str(self.__cur_id) + '-' + file_name
 
-        self.__config = ConfigParser.ConfigParser()
+        self.__cur_id += 1
+        self.__config.set(_DscStore.SYNC_SECTION, _DscStore.CUR_ID_OPTION,
+                          str(self.__cur_id))
+
+        return new_path
+
+    def remove(self):
+        if os.path.exists(self.__root_path):
+            shutil.rmtree(self.__root_path)
 
     def sync_container(self, container, items):
         print u'Syncing container  {0}...'.format(container['DisplayName'])
@@ -135,14 +200,25 @@ class _DscStore(object):
         for local in self.__removed_items(
                                     self.__config.options(container_id), items):
             if self.__config.has_section(local):
-                print u'\tRemoved container "{0}"' \
-                                .format(self.__config.get(container_id, local))
+                print u'\tRemoved a container'
                 self.__config.remove_option(container_id, local)
                 self.__config.remove_section(local)
             else:
-                print u'\tRemoved media "{0}"' \
-                                .format(self.__config.get(container_id, local))
+                orig = self.__config.get(container_id, local)
+                name = self.__config.get(container_id,
+                                              local + _DscStore.NAME_SUFFIX)
+                print u'\tRemoved media "{0}"'.format(name)
                 self.__config.remove_option(container_id, local)
+                self.__config.remove_option(container_id,
+                                            local + _DscStore.NAME_SUFFIX)
+                if local == orig:
+                    orig_name = self.__config.get(_DscStore.MEDIA_SECTION, orig)
+                    self.__config.remove_option(_DscStore.MEDIA_SECTION, orig)
+                    if self.__sync:
+                        print u'\tRemoved local downloaded contents "{0}"' \
+                                                            .format(orig_name)
+                        if os.path.exists(orig_name):
+                            os.remove(orig_name)
 
         self.__write_config()
 
@@ -160,6 +236,7 @@ class DscController(object):
     CONFIG_PATH = os.environ['HOME'] + '/.config/download-sync-controller.conf'
     SUID_OPTION = 'system_update_id'
     SRT_OPTION = 'service_reset_token'
+    SYNC_OPTION = 'sync_contents'
     DATA_PATH_SECTION = '__data_path__'
     DATA_PATH_OPTION = 'path'
 
@@ -176,7 +253,7 @@ class DscController(object):
             self.__set_data_path('download-sync-controller')
             
         self.__store_path = self.__config.get(DscController.DATA_PATH_SECTION,
-                                                DscController.DATA_PATH_OPTION)
+                                              DscController.DATA_PATH_OPTION)
 
     def __write_config(self):
         with open(DscController.CONFIG_PATH, 'wb') as configfile:
@@ -189,7 +266,7 @@ class DscController(object):
             self.__config.add_section(DscController.DATA_PATH_SECTION)
 
         self.__config.set(DscController.DATA_PATH_SECTION,
-                                    DscController.DATA_PATH_OPTION, data_path)
+                          DscController.DATA_PATH_OPTION, data_path)
 
         self.__write_config()
 
@@ -240,7 +317,7 @@ class DscController(object):
             print err
             return None
     
-    def track(self, server_path, track = True):
+    def track(self, server_path, track = True, sync_contents = True):
         server = Device(server_path)
         server_uuid = server.get_prop('UDN')
         
@@ -251,7 +328,13 @@ class DscController(object):
 
                 self.__config.set(server_uuid, DscController.SUID_OPTION, '-1')
                 self.__config.set(server_uuid, DscController.SRT_OPTION, srt)
-                
+                if sync_contents:
+                    self.__config.set(server_uuid, DscController.SYNC_OPTION,
+                                      'yes')
+                else:
+                    self.__config.set(server_uuid, DscController.SYNC_OPTION,
+                                      'no')
+
                 self.__write_config()
             else:
                 print u"Sorry, the server {0} has no such capability and " \
@@ -262,11 +345,11 @@ class DscController(object):
             self.__write_config()
             
             store = _DscStore(self.__store_path, server_uuid)
-            store.reset()
+            store.remove()
 
-    def track_reset(self, server):
-        self.track(server, False)
-        self.track(server)
+    def track_reset(self, server, sync_contents = True):
+        self.track(server, False, sync_contents)
+        self.track(server, True, sync_contents)
 
     def servers(self):
         print u'Running servers:'
@@ -304,18 +387,20 @@ class DscController(object):
 
         for item, uuid, cur, new, full_sync in \
                                     self.__need_sync(self.__upnp.get_servers()):
-            store = _DscStore(self.__store_path, uuid)
+            sync = self.__config.getboolean(uuid, DscController.SYNC_OPTION)
 
             if full_sync:
                 print u'Resetting local contents for server {0}'.format(uuid)
 
                 self.track_reset(item)
-                store.reset()
 
                 objects = _DscContainer(item).find_containers()
             else:
                 objects = _DscContainer(item).find_updates(cur)
-                
+
+            store = _DscStore(self.__store_path, uuid)
+            store.initialize(sync)
+
             for obj in objects:
                 if obj['Type'] == 'container':
                     children = _DscContainer(obj['Path']).find_children()
@@ -335,7 +420,7 @@ class DscController(object):
                 self.__config.remove_section(name)
 
                 store = _DscStore(self.__store_path, name)
-                store.reset()
+                store.remove()
 
         self.__write_config()
 
